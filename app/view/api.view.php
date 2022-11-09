f75abbf4c5724c75e5d19ef1aa8fc7aa0e14baf4
Agustin Gonzalorena <agusgonzaaa@hotmail.com>
2022-11-09 19:31:40 -0300

    problemas cors

--- app/view/api.view.php
+++ app/view/api.view.php
@@ -3,6 +3,9 @@
 class apiView{
     public function response($data, $status = 200) {
         header("Content-Type: application/json");
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
         header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
         
         // convierte los datos a un formato json
